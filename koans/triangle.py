3f892da8e4c79442aedcd64f223e0afa05050702
Shalane Proctor <shalane088@gmail.com>
2022-10-15 00:23:22 -0500

    still working

--- koans/triangle.py
+++ koans/triangle.py
@@ -17,8 +17,15 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    if a <= 0 or b <= 0 or c <= 0:
+        raise TriangleError(AttributeError('Negative edge is not allowed'))
+    if a + b + c <= 2 * max(a, b, c):
+        raise TriangleError(AttributeError('largest side should be smaller than sum of 2 other sides'))
+    if a == b == c:
+        return 'equilateral'
+    elif a == b or b == c or c == a:
+        return 'isosceles'
+    return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
